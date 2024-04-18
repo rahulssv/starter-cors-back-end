0499615e71ad310ffd6ee7985cbe959b8c0f3b49
Rahul Vishwakarma <rahulvishwakarma1181@gmail.com>
2024-04-18 07:11:13 +0000

    update

--- src/app.js
+++ src/app.js
@@ -9,6 +9,15 @@ const errorHandler = require("./errors/errorHandler");
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 app.use("/cors-enabled", corsEnabledRouter);
 app.use("/cors-not-enabled", corsNotEnabledRouter);
 
